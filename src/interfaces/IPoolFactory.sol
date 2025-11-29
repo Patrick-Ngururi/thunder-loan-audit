2322bbb1c54a6fbecad06492c8de8cd8914a2e3f
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2025-11-29 13:33:46 +0300

    Recon IPoolFactory

--- src/interfaces/IPoolFactory.sol
+++ src/interfaces/IPoolFactory.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: AGPL-3.0-only
 pragma solidity 0.8.20;
 
+// @Audit-Explainer: This is the interface for the TSwap PoolFactory.sol contract
+// @Audit-Question: Why are we using TSwap?
 interface IPoolFactory {
     function getPool(address tokenAddress) external view returns (address);
 }
