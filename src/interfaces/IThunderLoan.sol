08a6c507a02823396d9e0ae3b2b41bf25db5c186
Patrick-Ngururi <patrick9ngururi8@gmail.com>
2025-11-29 14:04:44 +0300

    Recon IThunderLoan.sol

--- src/interfaces/IThunderLoan.sol
+++ src/interfaces/IThunderLoan.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+// @Audit-Informational: The IThunderLoan interface should be implemented by the ThunderLoan contract!
 interface IThunderLoan {
+    // @Audit-Low/Informational: Incorrect parameter type being passed
     function repay(address token, uint256 amount) external;
 }
